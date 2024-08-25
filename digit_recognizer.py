f8d9618188c56070feee6136c67a8bd8010a239d
helenl12 <helenl12@cs.washington.edu>
2024-08-25 00:09:22 -0700

    Implement fetch requests so that predictions are displayed in chart

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,7 +1,5 @@
+import io, base64
 import numpy as np
-import tensorflow as tf
-import io
-import base64
 from flask import Flask, render_template, request, jsonify
 from tensorflow.keras.models import load_model
 from PIL import Image
@@ -11,8 +9,28 @@ app = Flask(__name__)
 # Load the trained model
 model = load_model("model.keras")
 
+# Convert canvas drawing image into array
+def convertImage(imageData):
+    imageBytes = base64.b64decode(imageData.split(',')[1])
+    image = Image.open(io.BytesIO(imageBytes))
+    # MNIST data (28x28), Luminance = grayscale(255 pixels)
+    image = image.resize((28, 28)).convert('L')
+    imageArray = np.array(image) / 255.0
+    imageArray = np.expand_dims(imageArray, axis=0) # fit input shape
+    return imageArray
+
+# Prediction route: runs model on input image
+@app.route('/_predict', methods=['POST'])
+def predict():
+    imageData = request.json['image_data']
+    imageArray = convertImage(imageData)
+    prediction = model.predict(imageArray)
+    return jsonify({'results': prediction.flatten().tolist()})
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-app.run(host="0.0.0.0", port=5000)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
---
+++ static/scripts.js
@@ -0,0 +1,103 @@
+let canvas, context, docChart, barChart;
+let X, Y, mouseDown = 0;
+let lastX, lastY = -1;
+
+
+function init() {
+    canvas = document.getElementById("sketchpad");
+    if (canvas.getContext) context = canvas.getContext("2d");
+    if (context) {
+        // draw w/ mouse click
+        canvas.addEventListener("mousedown", sketchpad_mouseDown);
+        canvas.addEventListener("mousemove", sketchpad_mouseMove);
+        window.addEventListener("mouseup", sketchpad_mouseUp);
+        // draw on touch screen
+        canvas.addEventListener("touchstart", sketchpad_mouseDown);
+        canvas.addEventListener("touchmove", sketchpad_mouseMove);
+        window.addEventListener("touchend", sketchpad_mouseUp);        
+    }
+    // initialize chart
+    docChart = document.getElementById("barChart");
+    var chartData = {
+        labels: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+        datasets: [{
+            label: 'Confidence Percentage',
+            data: [0,0,0,0,0,0,0,0,0,0],
+            borderWidth: 1
+        }]
+    };
+    barChart = new Chart(docChart, {
+        type: "bar",
+        data: chartData
+    });
+}
+
+/** event handlers for drawing **/
+function sketchpad_mouseDown(e) {
+    mouseDown=1; // boolean value True
+    getPos(e);
+    drawLine(context, X, Y);
+}
+function sketchpad_mouseMove(e) {
+    getPos(e);
+    if (mouseDown == 1) drawLine(context, X, Y);
+}
+function sketchpad_mouseUp() {
+    mouseDown = 0; // boolean value False
+    lastX = -1;
+    lastY = -1;
+}
+function clearCanvas() { // clear canvas
+    context.clearRect(0,0, canvas.width, canvas.height);
+}
+
+/** helper methods for drawing **/
+function drawLine(context, x, y) {
+    if (lastX == -1) {
+        lastX = x;
+        lastY = y;
+    }
+    context.strokeStyle = "white";
+    context.lineCap = "round";
+    context.lineWidth = 20;
+
+    context.beginPath();
+    context.moveTo(lastX,lastY);
+    context.lineTo(x,y);
+    context.stroke();
+
+    lastX = x;
+    lastY = y;
+}
+function getPos(e) {
+    if (e.offsetX) {
+        X = e.offsetX;
+        Y = e.offsetY;
+    } else if (e.touches) {
+        if (e.touches.length == 1) {
+            let touch = e.touches[0];
+            X = touch.clientX;
+            Y = touch.clientY;
+        }
+    }
+}
+
+/** predict the number using CNN **/
+function predict() {
+    let imageData = canvas.toDataURL();
+    fetch('/_predict', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ image_data: imageData })
+    })
+    .then(response => response.json())
+    .then(response => response.results) // get prediction probabilities
+    .then(predictions => {
+        // get CNN's guess (num w/ max probability)
+        let num = predictions.indexOf(Math.max(...predictions));
+        document.getElementById("predictionText").innerHTML = num;
+        // update bar chart w/ probabilities
+        barChart.data.datasets[0].data = predictions.map(confidence => confidence*100);
+        barChart.update();
+    })
+}
